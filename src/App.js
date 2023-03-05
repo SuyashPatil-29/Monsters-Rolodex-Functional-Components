1bcf608eb22969300a2670a39d5d09d974b628b6
SuyashPatil-29 <112413583+SuyashPatil-29@users.noreply.github.com>
2023-03-05 18:39:55 +0530

    optimized code to stop infinite re-rendering

--- src/App.js
+++ src/App.js
@@ -13,14 +13,12 @@ const App = ()=>{
   const [monsters , setMonsters] = useState([])
 
 
-  console.log("render");
 
   useEffect(()=>{
-    console.log("effect fired");
     fetch('https://jsonplaceholder.typicode.com/users')
    .then(response => response.json())
    .then((users)=> setMonsters(users))
-  })
+  }, [])
 
    
   const filteredMonsters =monsters.filter((monster)=>{
@@ -41,47 +39,5 @@ const App = ()=>{
     </div>
   )
 }
-
-
-// class App extends Component {
-//   constructor() {
-//     super();
-    
-//     this.state = {
-//       monsters: [],
-//       searchField: ''
-//     }
-
-//   }
-
-  //  componentDidMount(){
-  //    fetch('https://jsonplaceholder.typicode.com/users')
-  //      .then(response => response.json())
-  //      .then((users)=> this.setState(()=>{
-  //       return {monsters: users}
-  //      },
-  //      ()=>{
-  //       console.log(this.state);
-  //      })) 
-  //  }
-
-
-//   render(){
-//     const {monsters, searchField} = this.state
-//     const {onSearchChange } = this
-//     const filteredMonsters =monsters.filter((monster)=>{
-//        return monster.name.toLowerCase().includes(searchField)
-//     })
-
-
-//   return (
-//     <div className="App">
-//         <h1 className='app-title'>Monsters Rolodex</h1>
-//
-//         <CardList monsters={filteredMonsters} />
-//     </div>
-//   );
-// }
-// }
 
 export default App;
